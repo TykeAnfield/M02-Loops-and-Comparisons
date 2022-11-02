dfc157cb0a0ace0da4fd9668db247e89739f1683
TykeAnfield <116766114+TykeAnfield@users.noreply.github.com>
2022-11-01 19:31:18 -0500

    Update student_gpa.py

--- student_gpa.py
+++ student_gpa.py
@@ -2,24 +2,35 @@
 Author: Tyke Anfield
 Last Revision 11-1-2022
 Program name: Student_gpa.py
-Purpose: Takes input for students name, then input for GPA. if  GPA is equal or greater than 3.5 the program outputs the students name and declares they made it on the Dean's list,
+Purpose: Takes input for students names, then input for GPA. if GPA is equal or greater than 3.5 the program outputs the students name and declares they made it on the Dean's list,
 if  GPA is equal or greater than 3.25 the program outputs the students name and declares they made it on the Honor Roll.
 '''
 
-# Quits or breaks out of the program
+# Input Students last name or Quits the program
 lName = input("Enter students last name or ZZZ to quit: ")
 
 # loop to cycle through entering inputs for students name and GPA
 while lName != "ZZZ":
-    #inputs for students name
+    # Input students name
     fName = input("Enter students first name: ")
-    #input for GPA
+    # Input GPA
     gpa = float(input("Enter "+fName+" "+lName+"'s GPA: "))
-    # If loop to compare GPA input with  
+    
+    # Compare GPA inputs with default condition
     if gpa >= 3.5:
-    # output students name if they made the Dean's list
-        print(fName, lName, "has made the Dean's List")
+    # Output students name if they made the Dean's list
+        print(fName, lName, "has made the Dean's List.")
+        # empty string to create new line before starting new student
+        print("")
     elif gpa >= 3.25:
+        # Output students name if they made the Honor Roll
         print(fName, lName, "has made the Honor Roll.")
-    # Quits or breaks out of the program
+        # empty string to create new line before starting new student
+        print("")
+    else:
+        print(fName, lName, "did not made the Honor Roll or the Dean's list.")
+        # empty string to create new line before starting new student
+        print("")
+    # Input Students last name or Quits the program
     lName = input("Enter students last name or ZZZ to quit: ")
+    
